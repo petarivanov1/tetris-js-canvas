efbbb023a60ae4d5d3d8e510a911c2fd32693b36
Petar Ivanov <petariwwanoww@gmail.com>
2017-06-05 12:05:54 +0300

    Separate draw file

---
+++ js/draw.js
@@ -0,0 +1,38 @@
+(function () {
+    const canvas = document.getElementById("tetris-canvas");
+    const context = canvas.getContext('2d');
+
+    const requestAnimationFrame = (window.requestAnimationFrame
+        || window.mozRequestAnimationFrame
+        || window.webkitRequestAnimationFrame
+        || function (callback) {
+            setTimeout(callback, 1000 / 30); // 30 FPS
+        });
+
+    canvas.width = 800;
+    canvas.height = 600;
+
+    function drawGrid() {
+        for (let i = 0; i <= TETRIS_ROWS; i += 1) {
+            context.moveTo(0, TETRIS_CELL_SIZE * i);
+            context.lineTo(TETRIS_COLS * TETRIS_CELL_SIZE, TETRIS_CELL_SIZE * i);
+            context.stroke();
+        }
+
+        for (let i = 0; i <= TETRIS_COLS; i += 1) {
+            context.moveTo(TETRIS_CELL_SIZE * i, 0);
+            context.lineTo(TETRIS_CELL_SIZE * i, TETRIS_ROWS * TETRIS_CELL_SIZE);
+            context.stroke();
+        }
+    }
+
+    function draw() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawGrid();
+
+        requestAnimationFrame(draw);
+    }
+
+    draw();
+}());
