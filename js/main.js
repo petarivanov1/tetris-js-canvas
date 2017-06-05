b8c9d35a6cbba568e5d5521a2890c9cb81a03b22
Petar Ivanov <petariwwanoww@gmail.com>
2017-06-05 17:08:10 +0300

    Check for collision on rotation

--- js/main.js
+++ js/main.js
@@ -85,16 +85,16 @@ function getFigure() {
     currentFigure.col = 0;
 }
 
-function checkForCollision(offsetRow, offsetCol) {
-    for (let i = 0; i < currentFigure.obj.cells.length; i += 1) {
+function checkForCollision(offsetRow, offsetCol, matrix) {
+    for (let i = 0; i < matrix.length; i += 1) {
         const row = offsetRow + i;
         if (row < 0) {
             continue;
         };
-        for (let j = 0; j < currentFigure.obj.cells[i].length; j += 1) {
+        for (let j = 0; j < matrix[i].length; j += 1) {
             const col = offsetCol + j;
 
-            if (currentFigure.obj.cells[i][j] && tetrisTable[row][col]) {
+            if (matrix[i][j] && tetrisTable[row][col]) {
                 return true;
             }
         }
@@ -103,7 +103,7 @@ function checkForCollision(offsetRow, offsetCol) {
 }
 
 function update() {
-    let canFall = !checkForCollision(currentFigure.row + 1, currentFigure.col);
+    let canFall = !checkForCollision(currentFigure.row + 1, currentFigure.col, currentFigure.obj.cells);
 
     if (canFall) {
         currentFigure.row += 1;
@@ -129,32 +129,38 @@ function update() {
 getFigure();
 update();
 
-setInterval(function() {
-    gameSpeed  += 1;
+setInterval(function () {
+    gameSpeed += 1;
 }, 60 * 1000);
 
 window.addEventListener('keydown', function (ev) {
     if (ev.key === 'ArrowLeft') {
-        const canMove = currentFigure.col > 0 && !checkForCollision(currentFigure.row, currentFigure.col - 1);
+        const canMove = currentFigure.col > 0 && !checkForCollision(currentFigure.row, currentFigure.col - 1,
+            currentFigure.obj.cells);
         if (canMove) {
             currentFigure.col -= 1;
         }
     } else if (ev.key === 'ArrowRight') {
-        const canMove = currentFigure.col + currentFigure.obj.cells[0].length < TETRIS_COLS && !checkForCollision(currentFigure.row, currentFigure.col + 1);
+        const canMove = currentFigure.col + currentFigure.obj.cells[0].length < TETRIS_COLS &&
+            !checkForCollision(currentFigure.row, currentFigure.col + 1, currentFigure.obj.cells);
         if (canMove) {
             currentFigure.col += 1;
         }
-    } else if(ev.key === 'ArrowDown') {
+    } else if (ev.key === 'ArrowDown') {
         gameSpeedOverride = 50;
     }
 });
 
-window.addEventListener('keyup', function(ev) {
-    if(ev.key === 'ArrowDown') {
+window.addEventListener('keyup', function (ev) {
+    if (ev.key === 'ArrowDown') {
         gameSpeedOverride = 50;
-    } else if (ev.key === 'q') { // rotate counter-clockwise
-        currentFigure.obj.cells = getLeftRotation(currentFigure.obj.cells);
-    } else if (ev.key === 'w') { // rotate clockwise
-        currentFigure.obj.cells = getRightRotation(currentFigure.obj.cells);        
+    } else if (ev.key === 'q' || ev.key === 'w') {
+        const rotateFunc = (ev.key === 'q' ? getLeftRotation : getRightRotation);
+        const matrix = rotateFunc(currentFigure.obj.cells);
+
+        const canRotate = currentFigure.col >= 0 && currentFigure.col + matrix[0].length < TETRIS_COLS && !checkForCollision(currentFigure.row, currentFigure.col, matrix);
+        if (canRotate) {
+            currentFigure.obj.cells = matrix;
+        }
     }
 });
