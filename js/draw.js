a2e86ef620936127e9f944ea1d736f1d3ab5f5c0
Petar Ivanov <petariwwanoww@gmail.com>
2017-06-05 14:07:06 +0300

    Add falling squares

--- js/draw.js
+++ js/draw.js
@@ -12,16 +12,32 @@
     canvas.width = 800;
     canvas.height = 600;
 
+    function drawFigure() {
+        const { row, col, obj: { color, cells} } = currentFigure;
+
+        context.fillStyle = color;
+
+        for (let i = 0; i < cells.length; i += 1) {
+            for (let j = 0; j < cells[i].length; j += 1) {
+                if(!cells[i][j]) {
+                    continue;
+                }
+
+                context.fillRect(getCellX(col + i), getCellY(row + j), TETRIS_CELL_SIZE, TETRIS_CELL_SIZE);
+            }
+        }
+    }
+
     function drawTable() {
         for (let i = 0; i < TETRIS_ROWS; i += 1) {
             for (let j = 0; j < TETRIS_COLS; j += 1) {
-                if(!tetrisTable[i][j]) {
+                if (!tetrisTable[i][j]) {
                     continue;
                 }
 
                 const color = tetrisTable[i][j];
                 context.fillStyle = color;
-                context.fillRect(getCellX(i), getCellY(j), TETRIS_CELL_SIZE, TETRIS_CELL_SIZE);
+                context.fillRect(getCellY(j), getCellX(i), TETRIS_CELL_SIZE, TETRIS_CELL_SIZE);
             }
         }
     }
@@ -43,6 +59,7 @@
     function draw() {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
+        drawFigure();
         drawTable();
         drawGrid();
 
--- js/main.js
+++ js/main.js
@@ -2,24 +2,77 @@ const TETRIS_ROWS = 18;
 const TETRIS_COLS = 10;
 const TETRIS_CELL_SIZE = 23;
 
-const tetrisTable = Array.from({length: TETRIS_ROWS})
-    .map(() => Array.from({length: TETRIS_COLS}).map(() => false));
+const tetrisTable = Array.from({ length: TETRIS_ROWS })
+    .map(() => Array.from({ length: TETRIS_COLS }).map(() => false));
 
-console.log(tetrisTable);
+tetrisTable.push(Array.from({ length: TETRIS_COLS}).map(() => true));
+
+const figures = [
+    {
+        color: 'red',
+        cells: [
+            [1, 1],
+            [1, 1],
+        ]
+    },
+];
 
 let x = 10;
 let y = 10;
-
-function update() {
-    x += 1;
-}
-
-setInterval(update, 100);
 
 function getCellX(row) {
     return TETRIS_CELL_SIZE * row;
 }
 
 function getCellY(col) {
-    return TETRIS_CELL_SIZE *col;
+    return TETRIS_CELL_SIZE * col;
 }
+
+let currentFigure = {
+    obj: {},
+    row: 0,
+    col: 0
+};
+
+let gameSpeed = 100;
+
+function getFigure() {
+    const index = Math.random() * figures.length | 0;
+    currentFigure.obj = figures[index];
+    currentFigure.row = 0;
+    currentFigure.col = 0;
+}
+
+function update() {
+    let canFall = true;
+    for (let i = 0; i < currentFigure.obj.cells.length; i += 1) {
+        const row = currentFigure.row + i + 1;
+        for (let j = 0; j < currentFigure.obj.cells[i].length; j += 1) {
+            const col = currentFigure.col + j;
+
+            if (currentFigure.obj.cells[i][j] && tetrisTable[row][col]) {
+                canFall = false;
+                break;
+            }
+        }
+    }
+    if (canFall) {
+        currentFigure.row += 1;
+    } else {
+        for (let i = 0; i < currentFigure.obj.cells.length; i += 1) {
+            const row = currentFigure.row + i;
+            for (let j = 0; j < currentFigure.obj.cells[i].length; j += 1) {
+                const col = currentFigure.col + j;
+
+                if (currentFigure.obj.cells[i][j]) {
+                    tetrisTable[row][col] = currentFigure.obj.color;
+                }
+            }
+        }
+    }
+
+    setTimeout(update, gameSpeed);
+}
+
+getFigure();
+update();
